55b49020956da17e82846247619e4ae96f929a5d
Denilson Lima <denilsondslima@gmail.com>
2023-01-24 00:33:16 -0300

    css da página de boleto adicionado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Boleto from "./pages/checkout/boleto";
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/boleto" element={<Boleto/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
