6fd6c21ec39ea10a8679cfb962bbb0c5300defe9
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2023-01-25 21:33:27 -0300

    teste 2 alteração para teste de brach

--- src/pages/shoppingBag/shoppingBag.jsx
+++ src/pages/shoppingBag/shoppingBag.jsx
@@ -1,9 +1,22 @@
+import styled from "styled-components";
 import { Main } from "../../assets/styles";
 
 export default function ShoppingBag() {
     return (
         <Main>
-            olá
+            <Header>
+
+            </Header>
+            <Bag>
+
+            </Bag>
         </Main>
     )
 }
+
+const Header = styled.header`
+    width: 100%;
+    `
+const Bag = styled.div`
+    width: 100%;
+    `
